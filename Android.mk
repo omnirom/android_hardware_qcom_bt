60c34634389c983c60f36d6d3cb894f416cadca2
Jake Whatley <jacob.whatley91@gmail.com>
2016-11-20 15:25:40 -0500

    bt: add caf-msm8992
    Change-Id: Id632661931fa5c1eb8da462befb7783532787a07
    
    libbt-qcom: Allow building without proprietary bits
    
    Change-Id: I1a123e84064e441e7b4d3bf3437fa655f38f38c2
    
    libbt: Fix case where SoC type is not set
    
    Change-Id: Idbd953f2a17c426efb24b436b1933d4170732e54
    
    Bluetooth: load btaddr from NV if QCOM_BT_USE_BTNV is set
    
    - OSS builds never set QCPATH, so we should instead use libdl
      so devices that need to read btaddr from NV can still build
      from source.
    
    Change-Id: Ib38dc356c7a3458f57935d113e068e6637d4795f
    
    Fix up building with TARGET_2ND_ARCH
    
    Set up the intermediates path to generate the buildcfg header at
    the right location
    
    Change-Id: Ifd65a27134100ec40332bcc3b15d1950e3f43a7b

--- Android.mk
+++ Android.mk
@@ -2,7 +2,11 @@ ifneq ($(filter msm8960 msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
 include $(call all-named-subdir-makefiles,msm8960)
 else
 ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
-include $(call all-named-subdir-makefiles,msm8992)
+  ifneq ($(filter caf-msm8992 ,$(TARGET_QCOM_BLUETOOTH_VARIANT)),)
+    include $(call all-named-subdir-makefiles,$(TARGET_QCOM_BLUETOOTH_VARIANT))
+  else
+    include $(call all-named-subdir-makefiles,msm8992)
+  endif
 endif
 ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
 include $(call all-named-subdir-makefiles,msm8996)
