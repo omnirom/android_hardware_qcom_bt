e53e0d5a8f7dd0f3eb9a04475d9a50a101a77773
khanfrd <khanfrd94@gmail.com>
2018-07-06 18:27:29 +0530

    caf-msm8998: allow sdm660
    Change-Id: Iaa8f682ca71a7e704f71dde50aa177cda219d216

--- caf-msm8998/Android.mk
+++ caf-msm8998/Android.mk
@@ -1,3 +1,3 @@
-ifneq ($(filter msm8992 msm8994 msm8998,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8992 msm8994 msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
 include $(call all-named-subdir-makefiles,libbt-vendor)
 endif
