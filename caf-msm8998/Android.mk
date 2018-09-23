19ab9c61ec48b8a204142fd4910b723de3a7e6a0
Marko Man <darkobas@gmail.com>
2018-09-23 23:48:32 +0200

    import caf-msm8998  LA.UM.7.4.r1-03000-8x98.0
    Change-Id: I4afe83c64781f9358478781e441378502b57b5f9

--- caf-msm8998/Android.mk
+++ caf-msm8998/Android.mk
@@ -1,3 +1,16 @@
-ifneq ($(filter msm8992 msm8994 msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
-include $(call all-named-subdir-makefiles,libbt-vendor)
+ifeq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+TMP_LOCAL_PATH := $(LOCAL_PATH)
+
+include $(TMP_LOCAL_PATH)/libbt-vendor/Android.mk
+
+include $(TMP_LOCAL_PATH)/tools/Android.mk
+
+ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+include $(TMP_LOCAL_PATH)/bthost_ipc/Android.mk
+endif #TARGET_USE_QTI_BT_STACK
 endif
