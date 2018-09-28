c71e5b74df1b81527668c5c7b30800c0d3259509
Marko Man <darkobas@gmail.com>
2018-09-28 12:08:21 +0200

    bt: fix include
    move tools into QSSI
    
    Change-Id: I29d9e286b5f20dc8a6845c848f5a3b1aab03d30d

--- caf-msm8998/Android.mk
+++ caf-msm8998/Android.mk
@@ -3,8 +3,8 @@ ifneq ($(filter msm8992 msm8994 msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
  include $(CLEAR_VARS)
  TMP_LOCAL_PATH := $(LOCAL_PATH)
  include $(TMP_LOCAL_PATH)/libbt-vendor/Android.mk
- include $(TMP_LOCAL_PATH)/tools/Android.mk
  ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+ include $(TMP_LOCAL_PATH)/tools/Android.mk
 include $(TMP_LOCAL_PATH)/bthost_ipc/Android.mk
 endif #TARGET_USE_QTI_BT_STACK
 endif
